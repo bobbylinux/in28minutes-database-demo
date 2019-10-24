599d6aac491c8d57be67b4aac83cb8a006a7b6b6
bobbylinux <roberto.bani@gmail.com>
2019-10-24 20:44:36 +0200

    aggiunto insert in file data.sql per popolare al volo il database h2 che cancella i record quando viene stoppata la applicazione

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,8 @@ create table person
     birth_date timestamp,
     primary key (id)
 );
+
+INSERT INTO PERSON(ID, NAME, LOCATION, BIRTH_DATE) VALUES(10001, 'Roberto Bani', 'Firenze',sysdate);
+INSERT INTO PERSON(ID, NAME, LOCATION, BIRTH_DATE) VALUES(10002, 'Mario Rossi', 'Roma',sysdate);
+INSERT INTO PERSON(ID, NAME, LOCATION, BIRTH_DATE) VALUES(10003, 'Serena Verdi', 'Alessandria',sysdate);
+INSERT INTO PERSON(ID, NAME, LOCATION, BIRTH_DATE) VALUES(10004, 'Alessio Bianchi', 'Milano',sysdate);
