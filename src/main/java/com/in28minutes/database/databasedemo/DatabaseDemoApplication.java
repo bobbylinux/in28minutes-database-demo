5ece06a2bb1dc65d76e61f7979b8a15804f2a6d8
bobbylinux <roberto.bani@gmail.com>
2019-10-24 21:53:39 +0200

    aggiunti metodi per update ed insert

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,6 @@
 package com.in28minutes.database.databasedemo;
 
+import com.in28minutes.database.databasedemo.bean.Person;
 import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
@@ -25,5 +28,7 @@ public class DatabaseDemoApplication implements CommandLineRunner {
         logger.info("All users -> {}", personJdbcDAO.findAll());
         logger.info("users id 10001 -> {}", personJdbcDAO.findById(10001));
         logger.info("deleting 10002 -> number of rows deleted {}", personJdbcDAO.deleteById(10002));
+        logger.info("inserting 10005 -> {}", personJdbcDAO.insert(new Person(10005, "Riccardo Ricci", "Venezia", new Date())));
+        logger.info("updating 10003 ->  {}", personJdbcDAO.update(new Person(10003, "Francesco Neri", "Torino", new Date())));
     }
 }
--- src/main/java/com/in28minutes/database/databasedemo/bean/Person.java
+++ src/main/java/com/in28minutes/database/databasedemo/bean/Person.java
@@ -18,6 +18,38 @@ public class Person {
         this.birthDate = birthDate;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
     @Override
     public String toString() {
         return String.format("\nPerson [id=%s, name=%s, location=%s, birthDate=%s]", id, name, location, String.valueOf(birthDate));
